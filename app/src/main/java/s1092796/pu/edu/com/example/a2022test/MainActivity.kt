a8fd59df21809897698444fd078ca5df20cfaff7
elmer355023 <elmer355023@gmail.com>
2022-05-15 15:59:12 +0800

    v.2

--- app/src/main/java/s1092796/pu/edu/com/example/a2022test/MainActivity.kt
+++ app/src/main/java/s1092796/pu/edu/com/example/a2022test/MainActivity.kt
@@ -1,13 +1,47 @@
 package s1092796.pu.edu.com.example.a2022test
 
 import android.content.pm.ActivityInfo
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.*
+import s1092796.pu.edu.com.example.a2022test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img: ImageView
+    lateinit var mysv:MySurfaceView
+    lateinit var binding: ActivityMainBinding
+    var flag:Boolean = false
+    lateinit var job: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.img.setOnClickListener({
+            if (flag){
+                flag = false
+                binding.img.setImageResource(R.drawable.start)
+                job.cancel()
+            }
+            else{
+                flag = true
+                binding.img.setImageResource(R.drawable.stop)
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(flag) {
+                        delay(10)
+
+                        var canvas: Canvas = binding.mysv.surfaceHolder.lockCanvas()
+                            binding.mysv.drawSomething(canvas)
+                        binding.mysv.surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+
+            }
+        })
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
